48d0bfc31901a9242dc661b3d55d3a9f0ada4686
Josiah Saunders <yojo@icloud.com>
2020-06-28 14:23:01 -0600

    simplified recipe inherting

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,50 +1,24 @@
 part of dough;
 
 @immutable
-class DoughRecipe extends StatelessWidget {
+class DoughRecipe extends InheritedWidget {
   static final DoughRecipeData _kFallbackRecipe = DoughRecipeData.fallback();
 
-  final Widget child;
   final DoughRecipeData data;
 
   const DoughRecipe({
-    @required this.child,
-    this.data,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return _InheritedRecipe(
-      recipe: this,
-      child: child,
-    );
-  }
-
-  static DoughRecipeData of(BuildContext context) {
-    final ih = context.dependOnInheritedWidgetOfExactType<_InheritedRecipe>();
-    return ih?.recipe?.data ?? _kFallbackRecipe;
-  }
-}
-
-class _InheritedRecipe extends InheritedTheme {
-  final DoughRecipe recipe;
-
-  const _InheritedRecipe({
     Key key,
-    @required this.recipe,
     @required Widget child,
+    this.data,
   }) : super(key: key, child: child);
 
-  @override
-  Widget wrap(BuildContext context, Widget child) {
-    final ancestor = context.findAncestorWidgetOfExactType<_InheritedRecipe>();
-    return identical(this, ancestor)
-        ? child
-        : DoughRecipe(data: recipe.data, child: child);
+  static DoughRecipeData of(BuildContext context) {
+    final ih = context.dependOnInheritedWidgetOfExactType<DoughRecipe>();
+    return ih?.data ?? _kFallbackRecipe;
   }
 
   @override
-  bool updateShouldNotify(_InheritedRecipe old) {
-    return this.recipe.data != old.recipe.data;
+  bool updateShouldNotify(covariant DoughRecipe old) {
+    return this.data != old.data;
   }
 }
